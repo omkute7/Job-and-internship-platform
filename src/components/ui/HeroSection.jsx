ac815cfc5e86eedbdb8f72382521931219e3b011
omkute <omkute636@gmail.com>
2024-09-27 23:35:55 +0530

    Fixed Hydration Error

--- src/components/ui/HeroSection.jsx
+++ src/components/ui/HeroSection.jsx
@@ -4,25 +4,28 @@ import { Button } from './button'
 
 function HeroSection() {
   return (
-    <div className='w-full h-screen flex flex-col  items-center'>
-        {/* Hero Tag Line */}
-        <div className=' mt-32'>
-            <p
-             className=' text-8xl'
-             >Find 
-             <span className='text-blue-500 font-bold'>Jobs</span> and
-             <span className='text-blue-500 font-bold'>Internships</span><br/>Easy And Fast</p>
+    <div className='w-full h-screen flex flex-col  '>
+      {/* Hero Tag Line */}
+      <div className=' mt-32'>
+         <div
+          className=' text-6xl md:text-8xl'
+        >Find
+          <span className='text-blue-500 font-bold'>Jobs</span> and
+          <span className='text-blue-500 font-bold'>Internships</span><br />Easy And Fast</div> 
+      </div>
+      {/* Hero tagline description 1 line */}
+      <div className=' md:px-2'>
+        <p>Internsip Oppurtunities for College Students</p>
+      </div >
+      {/* Input Box for searchin Jobs [chadCn element] */}
+      <div className=' md:flex w-[60vh] mt-10  space-x-7 '>
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input className=" focus-visible:bg-none" type="text" placeholder="Search Job Here" />
+          <Button type="submit">Search</Button>
         </div>
-        {/* Hero tagline description 1 line */}
-        <div className=' items-start'>
-            <p>Internsip Oppurtunities for College Students</p>
-        </div >
-        {/* Input Box for searchin Jobs [chadCn element] */}
-        <div className='flex w-[60vh] mt-10 items-center space-x-3'>
-        <Input lablep />
-        <Button>Find Jobs</Button>
-        </div>
-       
+        {/* <Button className="m-3 " >Find Jobs</Button> */}
+      </div>
+
 
     </div>
   )
